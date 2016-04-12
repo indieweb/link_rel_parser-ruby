86b6a90004d71e0a0d5f60f1ca2a59c68ebcf777
Shane Becker <veganstraightedge@gmail.com>
2016-04-11 23:04:09 -0700

    Implement LinkRelParser.http_rels() to be used by LinkRelParser.parse()

--- lib/link_rel_parser.rb
+++ lib/link_rel_parser.rb
@@ -6,12 +6,13 @@ module LinkRelParser
   class << self
 
     def parse(headers, base_url: nil)
+      # TODO
       response = {
         status: "http_code",
-        type: "content_type",
-        rels: http_rels(headers, base_url: base_url)
+        type:   "content_type",
+        rels:   http_rels(headers, base_url: base_url)
       }
-      
+
       response[:rels].blank? ? {} : response
     end
 
@@ -23,13 +24,68 @@ module LinkRelParser
     # Examples
     #
     #   http_rels(headers)
-    #   # => 'TomTomTomTom'
+    #   # => { "x-pingback" => "http://www.example.com/xmlrpc.php" }
     #
     # Returns an array of key/value pairs of LINK name and REL value or an empty array
     def http_rels(headers, base_url: nil)
-      links_rels = [headers]
+      return [] if headers.blank?
+
+      # clean up and normal headers
+      headers = headers.strip.gsub(/\r\n|\r/, "\n").
+                              gsub(/\t+/, " ").
+                              gsub(/^\s+/, "").
+                              split("\n")
+
+      links_rels = {}
+      headers.each do |header|
+        header.sub!(/Link: /i, "Link: ")
+
+        if header =~ /X-Pingback:/
+          # convert to a Link header and have common code handle it
+          header = %Q{Link: <#{ header.split('X-Pingback: ').last }>; rel="pingback"}
+        end
+
+        if header =~ /Link:/
+          links = header.split("Link: ").last.strip.split(", ")
+          links.each do |link|
+            href_and_rel = link.split("; ")
+            href = href_and_rel.first.gsub(/<|>/, "")
+
+            rels = []
+            href_and_rel.each do |rel_value|
+              if rel_value =~ /rel=/
+                rels = rel_value.gsub(/"|'/, "").split("rel=").last.strip.split
+              end
+            end
+
+            # ignore Link: headers without rel
+            unless rels.blank?
+              rels.each do |rel|
+                unless rel.blank?
+                  rel = rel.downcase.strip
+
+                  if links_rels[rel].blank?
+                    links_rels[rel] = ""
+                  end
+
+                  unless base_url.blank?
+                    href = base_url + href # TODO use standard library for this
+                  end
+
+                  # TODO is this necessary?
+                  unless links_rels[rel].include?(href)
+                    links_rels[rel] << href
+                  end
+                end
+              end
+            end
+
+          end
+        end
+      end
 
       links_rels
     end
+
   end
 end
